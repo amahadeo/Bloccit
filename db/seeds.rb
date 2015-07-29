e1b32787431183f6fb8e4c444e9ec09f3f0b6515
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-07-29 21:41:09 +0000

    Assignment 32 completed, seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,11 @@ require 'faker'
 
 posts = Post.all
 
+if !posts.any? {|post| post.title == "Special Post"}
+  Post.create(title: "Special Post", body: "Special body for the special post!")
+end
+  
+
 # Create Comments
 100.times do
   Comment.create!(
@@ -17,6 +22,13 @@ posts = Post.all
     body: Faker::Lorem.paragraph
     )
   end
+  
+comments = Comment.all
+
+if !comments.any? {|comment| comment.body == "Special comment is written here!"}
+  Comment.create(post: posts.sample, body: "Special comment is written here!")
+end
+
   
 puts "Seed finished"
 puts "#{Post.count} posts created"
