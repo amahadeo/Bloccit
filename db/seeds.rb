b5e86781606778f115cd358e922f2f2b9cd49266
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-08-15 01:06:51 +0000

    Checkpoint 40 completed, topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 
-#Create Users
+# Create Users
 5.times do
   user = User.new(
     name:     Faker::Name.name,
@@ -20,11 +20,21 @@ users = User.all
 # to avoid triggering a confirmation email when the User is saved.
 
 # The 'save' method then saves this User to the database.
+
+# Create Topics
+15.times do
+  Topic.create!(
+    name:        Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
     
-#Create Posts
+# Create Posts
 50.times do 
   Post.create!(
     user:  users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
     )
@@ -72,6 +82,7 @@ member.save!
   
 puts "Seed finished"
 puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
   
