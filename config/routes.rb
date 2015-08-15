485186d3e03307ee78f3a3fd5f4b633e2690da8a
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-08-15 05:09:26 +0000

    Assignment 40 completed, topics

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index] do
+      resource :summary, except: [:edit, :update]
+    end
   end
 
   get 'about' => 'welcome#about'
