1da59347a6bb4107d740ecce2154e9cb734170fb
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-08-20 05:09:20 +0000

    Assignment 43 completed, markdown

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,29 @@
 class Post < ActiveRecord::Base
-    has_many :comments
-    belongs_to :user
-    belongs_to :topic
+  has_many :comments
+  belongs_to :user
+  belongs_to :topic
     
-    default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
     
-    validates :title, length: { minimum: 5 }, presence: true
-    validates :body, length: { minimum: 20 }, presence: true
-    validates :topic, presence: true
-    validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+  
+  def markdown_title
+    render_as_markdown(title)  
+  end
+  
+  def markdown_body
+    render_as_markdown(body)
+  end
+  
+  private
+  
+  def render_as_markdown(attribute)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer,extensions)
+    (redcarpet.render attribute).html_safe
+  end
 end
