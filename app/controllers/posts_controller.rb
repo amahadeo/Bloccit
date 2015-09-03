b2964006e803bea8faaa97c0e9afc8ece7899d7b
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-09-03 02:12:36 +0000

    Checkpoint 50 completed, RSpec Mocking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
     @post.user = current_user
     authorize @post
     if @post.save
+      @post.create_vote
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
